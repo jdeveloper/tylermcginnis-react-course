37948a533d5534ed28eeb0d44be213a6224378d1
jota <jdeveloper.inxenio@gmail.com>
2019-08-08 20:49:10 +0200

    Higlight selectect popular language

--- app/components/Popular.js
+++ app/components/Popular.js
@@ -1,13 +1,32 @@
 import React from 'react'
 
 class Popular extends React.Component {
+    constructor(props) {
+        super(props)
+
+        this.state = {
+            selectedLenguage: 'All'
+        }
+
+        this.updateLenguage = this.updateLenguage.bind(this)
+    }
+
+    updateLenguage(lenguage) {
+        this.setState({
+            selectedLenguage: lenguage
+        })
+    }
+
     render() {
         const lenguages = ['All', 'JavaScript', 'PHP', 'Ruby', 'Java', 'CSS', 'Python']
         return (
             <ul className='flex-center'>
                 {lenguages.map((lenguage) => (
                     <li key={lenguage}>
-                        <button className='btn-clear nav-link'>
+                        <button 
+                            className='btn-clear nav-link' 
+                            style={ lenguage === this.state.selectedLenguage ? {color: 'rgb(187, 42, 31)' } : null }
+                            onClick={() => this.updateLenguage(lenguage)}>
                             {lenguage}
                         </button>
                     </li>
