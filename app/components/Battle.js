da0497357e055f5f700b76f43c02ae00d5e5178f
jota <jdeveloper.inxenio@gmail.com>
2019-08-10 12:34:29 +0200

    Added PlayerInput component

--- app/components/Battle.js
+++ app/components/Battle.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import { FaUserFriends, FaFighterJet, FaTrophy } from 'react-icons/fa'
+import PropTypes from 'prop-types'
 
 function Instructions () {
     return (
@@ -25,11 +26,71 @@ function Instructions () {
     )
 }
 
+class PlayerInput extends React.Component {
+    constructor(props) {
+        super(props)
+
+        this.state = {
+            username: ''
+        }
+
+        this.handleSubmit = this.handleSubmit.bind(this)
+        this.handleChange = this.handleChange.bind(this)
+    }
+
+    handleChange(event) {
+        this.setState({
+            username: event.target.value
+        })
+    }
+
+    handleSubmit(event) {
+        event.preventDefault()
+
+        this.props.onSubmit(this.state.username)
+    }
+
+    render() {
+        return (
+            <form className='column player' onSubmit={this.handleSubmit}>
+                <label htmlFor='username' className='player-label'>
+                    {this.props.label}
+                </label>
+                <div className='row player-inputs'>
+                    <input
+                        type='text'
+                        id='username'
+                        className='input-light'
+                        placeholder='Github username'
+                        autoComplete='off'
+                        value={this.state.username}
+                        onChange={this.handleChange}
+                    />
+                    <button
+                        className='btn dark-btn'
+                        type='submit'
+                        disabled={!this.state.username}
+                    >
+                        Submit
+                    </button> 
+                </div>
+            </form>
+        )
+    }
+}
+
+PlayerInput.propTypes = {
+    label: PropTypes.string.isRequired,
+    onSubmit: PropTypes.func.isRequired
+}
+
 export default class Battle extends React.Component {
     render() {
         return (
             <React.Fragment>
                 <Instructions />
+
+                <PlayerInput label='Github username' onSubmit={(value) => alert(value)} />
             </React.Fragment>
         )
     }
