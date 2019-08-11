7606fee9e1a4ce4bdea0575c36bc93f39dc9190a
jota <jdeveloper.inxenio@gmail.com>
2019-08-11 18:09:42 +0200

    Render battle results

--- app/components/Popular.js
+++ app/components/Popular.js
@@ -34,7 +34,7 @@ function ReposGrid({ repos }) {
                 const { login, avatar_url } = owner
 
                 return (
-                    <li key={html_url} className="repo bg-light">
+                    <li key={html_url} className="card bg-light">
                         <h4 className='header-lg center-text'>
                             #{index + 1}
                         </h4>
@@ -132,7 +132,7 @@ class Popular extends React.Component {
 
                     {this.isLoading() && <p>LOADING</p>}
 
-                    {error && <p>{error}</p>}
+                    {error && <p className="centet-text error">{error}</p>}
 
                     {repos[selectedLenguage] && <ReposGrid repos={repos[selectedLenguage]} />}
                 </React.Fragment>
