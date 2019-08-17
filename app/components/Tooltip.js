08dd40a3ba34b29186f692ce02d37fe5641b51c2
jota <jdeveloper.inxenio@gmail.com>
2019-08-17 13:55:34 +0200

    Refactor Tooltip to use withHover component

--- app/components/Tooltip.js
+++ app/components/Tooltip.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import PropTypes from 'prop-types'
+import withHover from './withHover'
 
 const styles = {
   container: {
@@ -23,46 +24,18 @@ const styles = {
   }
 }
 
-export default class Tooltip extends React.Component {
-    constructor(props) {
-        super(props)
-
-        this.state = {
-            hovering: false
-        }
-
-        this.mouseOver = this.mouseOver.bind(this)
-        this.mouseOut = this.mouseOut.bind(this)
-    }
-
-    mouseOver(id) {
-        this.setState({
-            hovering: true
-        })
-    }
-
-    mouseOut(id) {
-        this.setState({
-            hovering: false
-        })
-    }
-
-    render() {
-        const { text, children } = this.props
-
-        return (
-            <div 
-                onMouseOver={this.mouseOver} 
-                onMouseOut={this.mouseOut} 
-                style={styles.container}
-            >
-              {this.state.hovering && <div style={styles.tooltip}>{text}</div>}
+function Tooltip ({text, children, hovering}) {
+    return (
+            <div style={styles.container}>
+              {hovering && <div style={styles.tooltip}>{text}</div>}
               {children}  
             </div>
         )
-    }
 }
 
 Tooltip.propTypes = {
-    text: PropTypes.string.isRequired
+    text: PropTypes.string.isRequired,
+    hovering: PropTypes.bool.isRequired
 }
+
+export default withHover(Tooltip)
