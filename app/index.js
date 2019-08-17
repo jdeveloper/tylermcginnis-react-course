8b29ddac92b9be719e27903bc2809b1a63032d46
jota <jdeveloper.inxenio@gmail.com>
2019-08-17 21:35:46 +0200

    Added theming

--- app/index.js
+++ app/index.js
@@ -2,16 +2,34 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import Popular from './components/Popular'
 import Battle from './components/Battle'
+import Nav from './components/Nav'
+import { ThemeProvider } from './contexts/theme'
 import './index.css'
 
 class App extends React.Component {
-    render() {
-        const name = 'Jota'
+    constructor(props) {
+        super(props)
 
+        this.state = {
+            theme: 'light',
+            toggleTheme: () => {
+                this.setState(({ theme }) => ({
+                   theme: theme === 'light' ? 'dark' : 'light' 
+                }))
+            }
+        }
+    }
+
+    render() {
         return (
-            <div className="container">
-                <Battle />
-            </div>
+            <ThemeProvider value={this.state}>
+                <div className={this.state.theme}>
+                    <div className="container">
+                        <Nav />
+                        <Battle />
+                    </div>
+                </div>
+            </ThemeProvider>
         )
     }
 }
