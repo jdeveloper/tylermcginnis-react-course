533b4126f4a66cfd14a838f5b8fd64d28c1f1a6b
jota <jdeveloper.inxenio@gmail.com>
2019-10-01 19:54:00 +0200

    Refactored Card.js and Nav.js with useContextº

--- app/components/Card.js
+++ app/components/Card.js
@@ -1,32 +1,30 @@
-import React from 'react'
-import { ThemeConsumer } from '../contexts/theme'
+import React, { useContext } from 'react'
+import ThemeContext from '../contexts/theme'
 
 export default function Card ({header, subheader, avatar, href, name, children }) {
+    const theme = useContext(ThemeContext)
+
     return (
-        <ThemeConsumer>
-          {(theme) => (
-            <div className={`card bg-${theme}`}>
-              <h4 className='header-lg center-text'>
-                {header}
-              </h4>
-              <img
-                className='avatar'
-                src={avatar}
-                alt={`Avatar for ${name}`}
-              />
-              {subheader && (
-                <h4 className='center-text'>
-                    {subheader}
-                </h4>
-              )}
-              <h2 className='center-text'>
-                <a className='link' href={href}>
-                  {name}
-                </a>
-              </h2>
-              {children} 
-            </div>
-          )}
-        </ThemeConsumer>
+      <div className={`card bg-${theme}`}>
+        <h4 className='header-lg center-text'>
+          {header}
+        </h4>
+        <img
+          className='avatar'
+          src={avatar}
+          alt={`Avatar for ${name}`}
+        />
+        {subheader && (
+          <h4 className='center-text'>
+              {subheader}
+          </h4>
+        )}
+        <h2 className='center-text'>
+          <a className='link' href={href}>
+            {name}
+          </a>
+        </h2>
+        {children} 
+      </div>
     )
 }
--- app/components/Nav.js
+++ app/components/Nav.js
@@ -1,5 +1,5 @@
-import React from 'react'
-import { ThemeConsumer } from '../contexts/theme'
+import React, { useContext } from 'react'
+import ThemeContext from '../contexts/theme'
 import { NavLink } from 'react-router-dom'
 
 const styles = {
@@ -9,40 +9,38 @@ const styles = {
 }
 
 export default function Nav ({toggleTheme}) {
+    const theme = useContext(ThemeContext)
+
     return (
-        <ThemeConsumer>
-            {(theme) => (
-                <nav className='row space-between'>
-                    <ul className="row nav">
-                        <li>
-                            <NavLink 
-                                to='/' 
-                                exact={true}
-                                className='nav-link'
-                                activeStyle={styles.activeLink}
-                            >
-                                Popular
-                            </NavLink>
-                        </li>
-                        <li>
-                            <NavLink 
-                                to='/battle' 
-                                className='nav-link'
-                                activeStyle={styles.activeLink}
-                            >
-                                Battle
-                            </NavLink>
-                        </li>
-                    </ul>
-                    <button
-                        style={{fontSize: 30}}
-                        className='btn-clear'
-                        onClick={toggleTheme}
+        <nav className='row space-between'>
+            <ul className="row nav">
+                <li>
+                    <NavLink 
+                        to='/' 
+                        exact={true}
+                        className='nav-link'
+                        activeStyle={styles.activeLink}
                     >
-                        {theme === 'light' ? '🔦' : '💡'}
-                    </button>
-                </nav>
-            )}
-        </ThemeConsumer>
+                        Popular
+                    </NavLink>
+                </li>
+                <li>
+                    <NavLink 
+                        to='/battle' 
+                        className='nav-link'
+                        activeStyle={styles.activeLink}
+                    >
+                        Battle
+                    </NavLink>
+                </li>
+            </ul>
+            <button
+                style={{fontSize: 30}}
+                className='btn-clear'
+                onClick={toggleTheme}
+            >
+                {theme === 'light' ? '🔦' : '💡'}
+            </button>
+        </nav>
     )
 }
